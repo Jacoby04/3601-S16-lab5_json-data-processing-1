fd2f1334e917122d07b2956ea2fc7b1c2c399fca
mitc0417 <mitc0417@morris.umn.edu>
2016-02-20 12:51:01 -0500

    student page and student controller and student view ZSAS

--- client/app/student/student.controller.js
+++ client/app/student/student.controller.js
@@ -1,6 +1,36 @@
 'use strict';
 
+(function() {
+
+  class StudentController {
+
+    constructor($http, $scope, socket) {
+      this.$http = $http;
+      this.awesomeThings = [];
+
+      $http.get('/api/students').then(response => {
+        this.awesomeThings = response.data;
+      socket.syncUpdates('student', this.awesomeThings);
+    });
+
+    $scope.$on('$destroy', function() {
+      socket.unsyncUpdates('student');
+    });
+  }
+
+  addThing() {
+    if (this.newThing) {
+      this.$http.post('/api/students', { name: this.newThing });
+      this.newThing = '';
+    }
+  }
+
+  deleteThing(thing) {
+    this.$http.delete('/api/students/' + thing._id);
+  }
+}
+
 angular.module('3601S16Lab5JsonDataProcessingApp')
-  .controller('StudentCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('StudentCtrl', StudentController);
+
+})();
